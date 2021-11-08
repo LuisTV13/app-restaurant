a7718cd46c7a0f05a4e76f2c06f17f437d9152eb
T5AM <i201914853@cibertec.edu.pe>
2021-11-08 18:27:45 -0500

    Commit Platillos

--- src/main/java/com/restaurant/service/PlatilloService.java
+++ src/main/java/com/restaurant/service/PlatilloService.java
@@ -1,7 +1,21 @@
 package com.restaurant.service;
 
+import java.util.List;
+import java.util.Optional;
 
+import com.restaurant.entity.Platillos;
 
 public interface PlatilloService   {
+	
+	public abstract List<Platillos> listarPlatillos();
+	
+	public abstract List<Platillos> listarPlatillosPorInicial( String detalle_pro);
+	
+	public abstract Platillos registrarActualizarPlatillo(Platillos obj); 
+	
+	public abstract void eliminarPlatillo (int codigo_pro);
+	
+	public Optional<Platillos> buscarPorId(int codigo_pro);
+	
 
 }
