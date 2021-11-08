1fc99d94b22cda5c13197628ca0264e1a7b5f4da
PIERRE <PIERRE@DESKTOP-1CL9FCC>
2021-11-08 00:47:57 -0500

    platillos

--- src/main/java/com/restaurant/controller/PlatillosController.java
+++ src/main/java/com/restaurant/controller/PlatillosController.java
@@ -1,5 +1,46 @@
 package com.restaurant.controller;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class PlatillosController {
+	
+	@RequestMapping("/verCrudPlatillos")
+	public String ver() {
+		return "crudPlatillos";
+	}
+	
+	@RequestMapping("/consultaCrudPlatillos")
+	public String consulta(Strign filtro, HttpSession session) {
+		List<Platillos> data = service.buscaporNombre(filtro +"%");
+		session.setAttribute("platillos", data)
+		return "crudPlatillos";
+	}
+	
+	@RequestMapping("/registraActualizaPlatillos")
+	public String registra(Platillos obj) {
+		service.registraActualizaPlatillos(obj);
+		session.setAttribute("Mensaje", "Registro Existoso")
+		return "redirec:salida";
+	}
+	
+	@RequestMapping("/eliminaCrudPlatillos")
+	public String elimina(Integer id, HttpSession session) {
+		service.eliminaPlatillos(obj);
+		session.setAttribute("Mensaje", "Eliminacion Existosa")
+		return "redirec:salida";
+	}
+	
+	
+	@RequestMapping("/salida")
+	public String listarTodos(HttpSession session) {
+		List<Platillos> data = service.listarTodos()
+		session.setAttribute("platillos", data)
+		return "crudPlatillos";
+	}
+	
 
 }
--- src/main/java/com/restaurant/repository/PlatillosRepository.java
+++ src/main/java/com/restaurant/repository/PlatillosRepository.java
@@ -1,9 +1,17 @@
 package com.restaurant.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;}
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.quey.Param;
 
 import com.restaurant.entity.Platillos;
 
+
 public interface PlatillosRepository extends JpaRepository<Platillos, Integer> {
+	
+	@Query("Select a from Platillos a where detalle_pro like :fil")
+	public abstract List<Alumno> buscarpornombre(@Param("fil")String filtro);
 
 }
